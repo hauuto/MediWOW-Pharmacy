73dfe457c5b5ec5475dd2172f371d3d98066abc6
QTru <23735771.tru@student.iuh.edu.vn>
2025-09-28 00:13:56 +0700

    God Diputs - Trueman

---
+++ src/main/java/com/gui/TruTest.java
@@ -0,0 +1,13 @@
+package com.gui;
+
+import javax.swing.*;
+
+public class TruTest {
+    private JPanel jpTru;
+    private JPanel jpTru2;
+    private JLabel lbTest3;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
