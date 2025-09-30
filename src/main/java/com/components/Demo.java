3fff13377232391f71ad748084ed31b2a31db27f
hauuto <thanhhau670@gmail.com>
2025-09-30 16:05:00 +0700

    add[component]: button

--- src/main/java/com/components/Demo.java
+++ src/main/java/com/components/Demo.java
@@ -8,6 +8,19 @@ public class Demo extends JFrame {
 
         JPanel panel = new JPanel();
         panel.add(new MediWOWButton("Bấm vào tôi"));
+        panel.add(new MediWOWButton("Primary",MediWOWTheme.PRIMARY));
+        panel.add(new MediWOWButton("Secondary",MediWOWTheme.SECONDARY));
+        panel.add(new MediWOWButton("Success",MediWOWTheme.SUCCESS));
+        panel.add(new MediWOWButton("Danger",MediWOWTheme.DANGER));
+        panel.add(new MediWOWButton("Warning",MediWOWTheme.WARNING));
+        panel.add(new MediWOWButton("Info",MediWOWTheme.INFO));
+        panel.add(new MediWOWButton("Accent",MediWOWTheme.ACCENT));
+        panel.add(new MediWOWButton("Light",MediWOWTheme.BACKGROUND));
+        panel.add(new MediWOWButton("Dark",MediWOWTheme.TEXT_DARK));
+        panel.add(new MediWOWButton("White",MediWOWTheme.WHITE));
+        panel.add(new MediWOWButton("Border",MediWOWTheme.BORDER));
+
+
 
         this.add(panel);
 
