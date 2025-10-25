428577ed6c2ee5e38d4bf227e4d754f1595264cb
QTru <23735771.tru@student.iuh.edu.vn>
2025-10-26 01:32:10 +0700

    feat(bus, enum): Added method calls in BUS_Product, Changed SOLID_ORAL_DOSAGE to LIQUID_DOSAGE in DosageForm

--- src/main/java/com/bus/BUS_Product.java
+++ src/main/java/com/bus/BUS_Product.java
@@ -1,4 +1,52 @@
 package com.bus;
 
-public class BUS_Product {
+import com.dao.DAO_Product;
+import com.entities.Lot;
+import com.entities.Product;
+import com.entities.UnitOfMeasure;
+import com.interfaces.IProduct;
+
+import java.util.List;
+
+public class BUS_Product implements IProduct {
+    private final DAO_Product daoProduct;
+
+    public BUS_Product() {
+        this.daoProduct = new DAO_Product();
+    }
+
+    @Override
+    public Product getProductById(String id) {
+        return daoProduct.getProductById(id);
+    }
+
+    @Override
+    public Lot getLotByBatchNumber(String batchNumber) {
+        return daoProduct.getLotByBatchNumber(batchNumber);
+    }
+
+    @Override
+    public List<Lot> getAllLots() {
+        return daoProduct.getAllLots();
+    }
+
+    @Override
+    public UnitOfMeasure getUnitOfMeasureById(String id) {
+        return daoProduct.getUnitOfMeasureById(id);
+    }
+
+    @Override
+    public List<UnitOfMeasure> getAllUnitOfMeasures() {
+        return daoProduct.getAllUnitOfMeasures();
+    }
+
+    @Override
+    public boolean addProduct(Product p) {
+        return daoProduct.addProduct(p);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return daoProduct.getAllProducts();
+    }
 }
